c4e3f46abba5c7ff07035b7463e69346f5a1da76
o.holienishcheva <o.holienishcheva@aimprosoft.com>
2020-09-23 18:55:19 +0300

    Updates

---
+++ precache-manifest.151ac775d57c4e986b1928d314cf24cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24366f28bc341f5a50ebbc3386860750",
+    "url": "/limehome-test/index.html"
+  },
+  {
+    "revision": "251f1cc5b8403d57b32e",
+    "url": "/limehome-test/static/css/main.34fd4dca.chunk.css"
+  },
+  {
+    "revision": "2504f1c2c75a0a545dc5",
+    "url": "/limehome-test/static/js/2.63380174.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/limehome-test/static/js/2.63380174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251f1cc5b8403d57b32e",
+    "url": "/limehome-test/static/js/main.601ef269.chunk.js"
+  },
+  {
+    "revision": "acc0afe4b22bdc288d95",
+    "url": "/limehome-test/static/js/runtime-main.5f1c4cf9.js"
+  }
+]);
